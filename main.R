015cf0cc4b96a4eed31db5777bf53f2d6a5535ae
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-07-31 11:40:56 +0800

    Add files via upload

--- main.R
+++ main.R
@@ -3,6 +3,13 @@
 # Created by: JE-Chen
 # Created on: 2020/7/31
 install.packages('wordcloud2')
+install.packages('webshot')
+install.packages('htmlwidgets')
 library('wordcloud2')
-text_data = read.csv('Test.csv')
-wordcloud2(text_data,0.5)
+library('webshot')
+library('htmlwidgets')
+webshot::install_phantomjs()
+text_data <- read.csv('Test.csv')
+text_png <- wordcloud2(text_data,0.5)
+saveWidget(text_png,"Text.html",selfcontained = F)
+webshot("Text.html","Clound.png", delay =5, vwidth = 480, vheight=480)
